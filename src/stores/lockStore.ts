6aa3c032b389d8498af78a4cb391b3c4cabe550a
zongqir <zongqir@qq.com>
2025-09-13 22:33:45 +0800

    [FIX] 默认不锁定

---
+++ src/stores/lockStore.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2025, ebAobS . All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  ebAobS designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by ebAobS in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/**
+ * ========================================
+ * 全局锁定状态管理
+ * ========================================
+ * 
+ * 管理整个应用程序的锁定状态，确保所有组件的锁定状态同步
+ */
+
+import { writable } from 'svelte/store';
+
+// 全局锁定状态 - 默认不锁定，允许直接编辑
+export const isLocked = writable<boolean>(true);
+
+// 锁定切换函数
+export function toggleLock() {
+  isLocked.update(locked => !locked);
+}
